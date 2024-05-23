23e34deb64e8a26b2af5370d3482a1686b4534d4
Judah Ben-Hur F de Aragão <jbhfaragao@cgm.pma.rima>
2024-05-23 14:13:33 -0300

    Streaming real-time Binance data. Just test!

--- stream.js
+++ stream.js
@@ -1,17 +1,38 @@
 const WebSocket = require('ws');
-const ws = new WebSocket("wss://stream.binance.com:9443/ws/!bookTicker");
- 
+
+// Primeira conexão para obter os símbolos
+const wsMiniTicker = new WebSocket("wss://stream.binance.com:9443/ws/!miniTicker@arr");
+
 const BOOK = {};
- 
-ws.onmessage = async (event) => {
-    const obj = JSON.parse(event.data);
-    BOOK[obj.s] = { ask: parseFloat(obj.a), bid: parseFloat(obj.b) }
+
+// Função para criar conexões WebSocket para cada símbolo
+function createSymbolWebSocket(symbol) {
+    const wsSymbol = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@trade`);
+
+    wsSymbol.onmessage = (event) => {
+        const tradeData = JSON.parse(event.data);
+        BOOK[symbol] = { price: parseFloat(tradeData.p) };
+        // console.log(BOOK);
+    };
 }
- 
-function getBook(symbol){
+
+// Quando a primeira conexão recebe uma mensagem
+wsMiniTicker.onmessage = (event) => {
+    const tickerData = JSON.parse(event.data);
+
+    tickerData.forEach((ticker) => {
+        const symbol = ticker.s;
+        if (!BOOK[symbol]) {
+            createSymbolWebSocket(symbol);
+        }
+    });
+};
+
+// Exportar função para obter dados em tempo real
+function getBook(symbol) {
     return BOOK[symbol];
 }
- 
+
 module.exports = {
     getBook
-}
+};
